548d14633363425fea2da3557c4a328b62d29505
gawadaz <gawad.azzam@gmail.com>
2018-10-20 12:40:04 +0200

    This is a new commit for what I originally planned to be amended

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = 'AIzaSyDd_RZ0zfN-tyOxIIUwc0rw-ykgSEsukAU';
+export const FIREBASE_AUTH_DOMAIN = 'myalobox-bb37a.firebaseapp.com';
+export const FIREBASE_DATABASE_URL = 'https://myalobox-bb37a.firebaseio.com';
+export const FIREBASE_PROJECT_ID = 'myalobox-bb37a';
+export const FIREBASE_STORAGE_BUCKET = 'myalobox-bb37a.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '462675052176';
+
+// export const FACEBOOK_APP_ID = <YOUR_FACEBOOK_APP_ID>;
